dc6124719cdb84e3bc6847e011064c132c00ac2e
HaiGH-Space <nthai0932169055@gmail.com>
2026-02-01 22:03:09 +0700

    feat(workspace): add WorkspaceEntity and update workspace controller with API response

---
+++ src/workspace/entities/workspace.entity.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Workspace } from 'generated/prisma/client';
+
+export class WorkspaceEntity implements Workspace {
+  @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000' })
+  id: string;
+
+  @ApiProperty({ example: 'Team Dev SyncFlow', description: 'Name of the workspace' })
+  name: string;
+
+  @ApiProperty({ example: 'team-dev-sf', description: 'Unique URL slug' })
+  urlSlug: string;
+
+  @ApiProperty({ example: 'user-uuid-123' })
+  ownerId: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+}
--- src/workspace/workspace.controller.ts
+++ src/workspace/workspace.controller.ts
@@ -4,10 +4,11 @@ import { CreateWorkspaceDto } from './dto/create-workspace.dto';
 import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
 import { WorkspaceRolesGuard } from 'src/common/guards/workspace-roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from 'src/common/decorators/user.decorator';
 import { Role, type User } from 'generated/prisma/client';
 import { SessionAuthGuard } from 'src/common/guards/session.guard';
+import { WorkspaceEntity } from './entities/workspace.entity';
 
 @ApiTags('Workspace')
 @Controller('workspace')
@@ -16,6 +17,10 @@ export class WorkspaceController {
   constructor(private readonly workspaceService: WorkspaceService) { }
 
   @Get('me')
+  @ApiOkResponse({ 
+    description: 'Get all workspaces for the current user', 
+    type: WorkspaceEntity 
+  })
   findAllByUserId(@CurrentUser() user: User) {
     return this.workspaceService.findAllByUserId(user.id);
   }
