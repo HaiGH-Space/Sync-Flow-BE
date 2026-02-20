64d407f0403776cd813b31bdcc9cd05edee29408
HaiGH-Space <nthai0932169055@gmail.com>
2026-02-20 22:10:54 +0700

    feat(issue): enhance issue controller with guards and response decorators

--- src/issue/issue.controller.ts
+++ src/issue/issue.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { IssueService } from './issue.service';
 import { CreateIssueDto } from './dto/create-issue.dto';
+import { ApiCommonErrors, ApiCreatedResponseGeneric } from 'src/common/decorators/api-common-responses.decorator';
+import { SessionAuthGuard } from 'src/common/guards/session.guard';
+import { ApiTags } from '@nestjs/swagger';
+import { IssueEntity } from './entities/issue.entity';
 
+@ApiTags('Issue')
 @Controller('issues')
+@UseGuards(SessionAuthGuard)
+@ApiCommonErrors()
 export class IssueController {
   constructor(private readonly issueService: IssueService) {}
 
   @Post()
+  @ApiCreatedResponseGeneric(IssueEntity)
   create(@Body() createIssueDto: CreateIssueDto) {
     return this.issueService.create(createIssueDto);
   }
