15420e321a2a4817a3a55b576d052a21d5084f1c
HaiGH-Space <nthai0932169055@gmail.com>
2026-02-24 18:50:54 +0700

    feat(sprint): enhance SprintController and SprintService with CRUD operations

--- src/sprint/sprint.controller.ts
+++ src/sprint/sprint.controller.ts
@@ -1,22 +1,46 @@
-import { Body, Controller, Post, UseGuards} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { SprintService } from './sprint.service';
 import { ApiTags } from '@nestjs/swagger';
-import { ApiCommonErrors } from 'src/common/decorators/api-common-responses.decorator';
+import { ApiCommonErrors, ApiCreatedResponseGeneric, ApiOkResponseGeneric } from 'src/common/decorators/api-common-responses.decorator';
 import { SessionAuthGuard } from 'src/common/guards/session.guard';
 import { ProjectAccessGuard } from 'src/common/guards/project-access.guard';
 import { CreateSprintDto } from './dto/create-sprint.dto';
 import { CurrentProject } from 'src/common/decorators/project.decorator';
-import type { Project } from 'generated/prisma/client';
+import { Role, type Project } from 'generated/prisma/client';
+import { SprintEntity } from './entities/sprint.entity';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UpdateSprintDto } from './dto/update-sprint.dto';
 
 @Controller('projects/:projectId/sprints')
 @ApiTags('Sprints')
 @ApiCommonErrors()
 @UseGuards(SessionAuthGuard, ProjectAccessGuard)
 export class SprintController {
-  constructor(private readonly sprintService: SprintService) {}
+  constructor(private readonly sprintService: SprintService) { }
 
   @Post()
+  @Roles(Role.ADMIN)
+  @ApiCreatedResponseGeneric(SprintEntity)
   create(@Body() dto: CreateSprintDto, @CurrentProject() project: Project) {
     return this.sprintService.create(dto, project.id);
+  }
+  @Get()
+  @ApiOkResponseGeneric(SprintEntity, true)
+  findAll(@Param('projectId') projectId: string) {
+    return this.sprintService.findAll(projectId);
+  }
+
+  @Delete(':sprintId')
+  @Roles(Role.ADMIN)
+  @ApiOkResponseGeneric(SprintEntity)
+  delete(@Param('sprintId') sprintId: string) {
+    return this.sprintService.delete(sprintId);
+  }
+
+  @Patch(':sprintId')
+  @Roles(Role.ADMIN)
+  @ApiOkResponseGeneric(SprintEntity)
+  update(@Param('sprintId') sprintId: string, @Body() dto: UpdateSprintDto) {
+    return this.sprintService.update(sprintId, dto);
   }
 }
--- src/sprint/sprint.service.ts
+++ src/sprint/sprint.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/_prisma/prisma.service';
 import { CreateSprintDto } from './dto/create-sprint.dto';
-// import { CreateSprintDto } from './dto/create-sprint.dto';
+import { UpdateSprintDto } from './dto/update-sprint.dto';
 
 @Injectable()
 export class SprintService {
@@ -12,6 +12,28 @@ export class SprintService {
         ...createSprintDto,
         projectId: projectId
       }
+    })
+  }
+  async findAll(projectId: string) {
+    return this.prisma.sprint.findMany({
+      where: {
+        projectId: projectId
+      }
+    })
+  }
+  async delete(sprintId: string) {
+    return this.prisma.sprint.delete({
+      where: {
+        id: sprintId
+      }
+    })
+  }
+  async update(sprintId: string, data: UpdateSprintDto) {
+    return this.prisma.sprint.update({
+      where: {
+        id: sprintId
+      },
+      data: data
     })
   }
 }
