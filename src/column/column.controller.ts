d90e3ff7914d8b5b0200bde3c6cc703d4974f9da
HaiGH-Space <nthai0932169055@gmail.com>
2026-03-07 21:03:56 +0700

    feat(column): implement create and find all columns functionality with DTO validation

--- src/column/column.controller.ts
+++ src/column/column.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ColumnService } from './column.service';
+import { SessionAuthGuard } from 'src/common/guards/session.guard';
+import { ProjectAccessGuard } from 'src/common/guards/project-access.guard';
+import { ApiCommonErrors, ApiCreatedResponseGeneric, ApiOkResponseGeneric } from 'src/common/decorators/api-common-responses.decorator';
+import { ApiTags } from '@nestjs/swagger';
+import { CurrentProject } from 'src/common/decorators/project.decorator';
+import { ColumnEntity } from './entities/column.entity';
+import type { Project } from 'generated/prisma/client';
+import { CreateColumnDto } from './dto/create-column.dto';
 
-@Controller('column')
+@ApiTags('Columns')
+@Controller('projects/:projectId/columns')
+@UseGuards(SessionAuthGuard, ProjectAccessGuard)
+@ApiCommonErrors()
 export class ColumnController {
-  constructor(private readonly columnService: ColumnService) {}
+  constructor(private readonly columnService: ColumnService) { }
+  @Get()
+  @ApiOkResponseGeneric(ColumnEntity, true)
+  findAll(@CurrentProject() project: Project) {
+    return this.columnService.findAll(project.id);
+  }
+
+  @Post()
+  @ApiCreatedResponseGeneric(ColumnEntity)
+  create(@CurrentProject() project: Project,@Body() dto: CreateColumnDto) {
+    return this.columnService.create(project.id, dto);
+  }
 }
--- src/column/column.service.ts
+++ src/column/column.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/_prisma/prisma.service';
+import { CreateColumnDto } from './dto/create-column.dto';
 
 @Injectable()
-export class ColumnService {}
+export class ColumnService {
+    constructor(private readonly prisma: PrismaService) { }
+    findAll(projectId: string) {
+        return this.prisma.column.findMany({
+            where: {
+                projectId,
+            },
+            orderBy: {
+                order: 'asc',
+            },
+        });
+    }
+    create(projectId: string, dto: CreateColumnDto) {
+        return this.prisma.column.create({
+            data: {
+                name: dto.name,
+                order: dto.order,
+                projectId
+            },
+        });
+    }
+}
