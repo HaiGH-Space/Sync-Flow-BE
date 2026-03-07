6a5e95fbac2bccbdd02ebcef07ed7d1da2dd1e0b
HaiGH-Space <nthai0932169055@gmail.com>
2026-03-07 21:10:46 +0700

    feat(column): refactor ColumnService to use async/await and handle errors in create method

--- src/column/column.service.ts
+++ src/column/column.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/_prisma/prisma.service';
 import { CreateColumnDto } from './dto/create-column.dto';
+import { Prisma } from 'generated/prisma/client';
 
 @Injectable()
 export class ColumnService {
     constructor(private readonly prisma: PrismaService) { }
-    findAll(projectId: string) {
+    async findAll(projectId: string) {
         return this.prisma.column.findMany({
             where: {
                 projectId,
@@ -15,13 +16,25 @@ export class ColumnService {
             },
         });
     }
-    create(projectId: string, dto: CreateColumnDto) {
-        return this.prisma.column.create({
-            data: {
-                name: dto.name,
-                order: dto.order,
-                projectId
-            },
-        });
+    async create(projectId: string, dto: CreateColumnDto) {
+        try {
+            return await this.prisma.column.create({
+                data: {
+                    name: dto.name,
+                    order: dto.order,
+                    projectId
+                },
+            });
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ConflictException('Order already exists in this project');
+                }else {
+                    throw new InternalServerErrorException('An error occurred');
+                }
+            }
+
+            throw new InternalServerErrorException('An error occurred');
+        }
     }
 }
