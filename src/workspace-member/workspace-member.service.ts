96d85e64a08251e466abb4bdfd9db2d8baa7c1c5
HaiGH-Space <nthai0932169055@gmail.com>
2026-03-17 21:04:11 +0700

    feat(user-profile): include emailVerified field in UserProfileEntity feat(workspace-member): add emailVerified field selection in findAllUsersInWorkspace

--- src/workspace-member/workspace-member.service.ts
+++ src/workspace-member/workspace-member.service.ts
@@ -14,6 +14,7 @@ export class WorkspaceMemberService {
             name: true,
             email: true,
             image: true,
+            emailVerified: true,
           }
         }
       }
