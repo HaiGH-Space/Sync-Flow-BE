a48db2b873d0ff8c76acc2a3a4524f8eabe8510e
HaiGH-Space <nthai0932169055@gmail.com>
2026-06-12 12:57:34 +0700

    feat(health): implement HealthController and HealthModule

---
+++ src/modules/health/health.controller.spec.ts
@@ -0,0 +1,61 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HealthController } from './health.controller';
+import { HealthCheckService } from '@nestjs/terminus';
+import { PrismaHealthIndicator } from './prisma.health';
+
+describe('HealthController', () => {
+  let controller: HealthController;
+  let healthService: HealthCheckService;
+  let prismaIndicator: PrismaHealthIndicator;
+
+  const mockHealthCheckService = {
+    check: jest.fn((checks) => {
+      return Promise.all(checks.map((c) => c())).then((results) => {
+        return {
+          status: 'ok',
+          info: Object.assign({}, ...results),
+          error: {},
+          details: Object.assign({}, ...results),
+        };
+      });
+    }),
+  };
+
+  const mockPrismaHealthIndicator = {
+    isHealthy: jest.fn().mockResolvedValue({
+      database: { status: 'up' },
+    }),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: mockHealthCheckService,
+        },
+        {
+          provide: PrismaHealthIndicator,
+          useValue: mockPrismaHealthIndicator,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<HealthController>(HealthController);
+    healthService = module.get<HealthCheckService>(HealthCheckService);
+    prismaIndicator = module.get<PrismaHealthIndicator>(PrismaHealthIndicator);
+  });
+
+  it('should call health check with prisma health check indicator', async () => {
+    const result = await controller.check();
+    expect(healthService.check).toHaveBeenCalled();
+    expect(prismaIndicator.isHealthy).toHaveBeenCalledWith('database');
+    expect(result).toEqual({
+      status: 'ok',
+      info: { database: { status: 'up' } },
+      error: {},
+      details: { database: { status: 'up' } },
+    });
+  });
+});
