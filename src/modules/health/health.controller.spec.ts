0877b1803aba1f44d697a8b0260b51a8db2b82db
HaiGH-Space <nthai0932169055@gmail.com>
2026-06-12 12:58:36 +0700

    test(health): fix lint errors in health spec files

--- src/modules/health/health.controller.spec.ts
+++ src/modules/health/health.controller.spec.ts
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
 import { HealthCheckService } from '@nestjs/terminus';
--- src/modules/health/prisma.health.spec.ts
+++ src/modules/health/prisma.health.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaHealthIndicator } from './prisma.health';
 import { PrismaService } from '../../database/prisma/prisma.service';
