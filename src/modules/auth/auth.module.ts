ddc41bd2eb0122597274ba48635ccb86baf1e6cc
HaiGH-Space <nthai0932169055@gmail.com>
2026-06-13 19:02:03 +0700

    feat: register SessionCleanupService in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { SessionAuthGuard } from "src/common/guards/session.guard";
+import { SessionCleanupService } from "./session-cleanup.service";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, SessionAuthGuard],
+  providers: [AuthService, SessionAuthGuard, SessionCleanupService],
 })
 export class AuthModule {}
