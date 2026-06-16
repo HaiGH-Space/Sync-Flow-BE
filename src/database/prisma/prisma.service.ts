dc4f54ba0e554e088e9662b483f9e7d15b2cec24
HaiGH-Space <nthai0932169055@gmail.com>
2026-06-16 19:22:27 +0700

    refactor(prisma): use NestJS Logger instead of console.log for db connection message

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { PrismaClient } from "generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { AppConfigService } from "src/config/config.service";
+
 @Injectable()
 export class PrismaService extends PrismaClient {
+  private readonly logger = new Logger(PrismaService.name);
+
   constructor(configService: AppConfigService) {
     const databaseUrl = configService.databaseUrl;
     const adapter = new PrismaPg({ connectionString: databaseUrl });
-    console.log("[🐛] Connected to the database");
     super({ adapter });
+    this.logger.log("[🐛] Connected to the database");
   }
 }
