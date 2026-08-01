027493612e2b29ad06c59aaf3dfdb8f9711712b1
HaiGH-Space <nthai0932169055@gmail.com>
2026-08-01 18:42:41 +0700

    feat: initialize global AuthModule with JWT configuration and session management providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
@@ -8,6 +8,7 @@ import { AppConfigModule } from "src/config/config.module";
 import { AppConfigService } from "src/config/config.service";
 import { SessionTokenService } from "./session-token.service";
 
+@Global()
 @Module({
   imports: [
     AppConfigModule,
